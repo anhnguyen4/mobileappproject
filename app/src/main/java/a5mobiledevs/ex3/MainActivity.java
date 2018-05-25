b90b8a0ff345ec5d690056ddbf9c27cb44c2b369
anhnguyen4 <37012012+anhnguyen4@users.noreply.github.com>
2018-05-26 00:50:10 +0700

    Parsed job titles from a sample page

--- app/src/main/java/a5mobiledevs/ex3/MainActivity.java
+++ app/src/main/java/a5mobiledevs/ex3/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -16,6 +19,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button getBtn;
     private TextView result;
     private TextView mTextMessage;
 
@@ -44,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        result = (TextView) findViewById(R.id.result);
+        getBtn = (Button) findViewById(R.id.button);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWebsite();
+            }
+        });
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -56,16 +69,18 @@ public class MainActivity extends AppCompatActivity {
                 final StringBuilder builder = new StringBuilder();
 
                 try {
-                    Document doc = Jsoup.connect("http://www.ssaurel.com/blog").get();
+                    Document doc = Jsoup.connect("http://sieuthivieclam.vn/tim-viec-lam/tim-viec-lam/category_4-ngan-hang-tu-van-tai-chinh/district_16-huyen-can-gio.html").get();
                     String title = doc.title();
-                    Elements links = doc.select("a[href]");
-
                     builder.append(title).append("\n");
+                    for (Element element : doc.select("a[href^=viec-lam]")) {
+                        builder.append("\n").append("Job title: ").append(element.attr("title"));
+                    }
+                    /*Elements links = doc.select("a[href]");
 
                     for (Element link : links) {
                         builder.append("\n").append("Link : ").append(link.attr("href"))
                                 .append("\n").append("Text : ").append(link.text());
-                    }
+                    }*/
                 } catch (IOException e) {
                     builder.append("Error : ").append(e.getMessage()).append("\n");
                 }
@@ -73,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        Log.i("LOL", builder.toString());
                         result.setText(builder.toString());
                     }
                 });
