58d28349995c4e6c9bee5f26bf2f180dbde7a1df
anhnguyen4 <37012012+anhnguyen4@users.noreply.github.com>
2018-05-26 15:06:23 +0700

    Included code to parse all job info

--- app/src/main/java/a5mobiledevs/ex3/MainActivity.java
+++ app/src/main/java/a5mobiledevs/ex3/MainActivity.java
@@ -70,17 +70,42 @@ public class MainActivity extends AppCompatActivity {
 
                 try {
                     Document doc = Jsoup.connect("http://sieuthivieclam.vn/tim-viec-lam/tim-viec-lam/category_4-ngan-hang-tu-van-tai-chinh/district_16-huyen-can-gio.html").get();
+                    /*
+<div class="job-item ">
+    <div class="row">
+        <div class="col-md-3 col-sm-3 col-xs-3 job-logo-col">
+              <div class="job-logo">
+                    <div class=" img-avatar  bg "   style="background-image:url(http://sieuthivieclam.vn/templates/sieuthivieclam/images/avatar.jpg)"></div>														</div>
+                  </div>
+                  <div class="col-md-9 col-sm-9 col-xs-9 job-info-col">
+                    <div class="job-info">
+                      <h3 class="title-job"><a title="Chuyên viên tín dụng" target="_blank" href="viec-lam/chuyen-vien-tin-dung-CV2017-A1947.html">Chuyên viên tín dụng </a></h3>
+                      <p class="company-name"><a href="cong-ty/ngan-hang-anz-viet-nam-dn2017-40939.html" class="text-muted" title="NGÂN HÀNG ANZ VIỆT NAM" target="_blank" >NGÂN HÀNG ANZ VIỆT NAM</a></p>
+
+                      <p class="save-job"><a alias="chuyen-vien-tin-dung-CV2017-A1947" title="Lưu công việc" class="ufav " href="#"><i class="fa fa-heart-o"></i></a></p>
+                      <div class="more-info clearfix">
+                        <div class="more-info-div salary-info">Lương: Thoả thuận</div>
+                        <div class="more-info-div city-info"><span class="pe-7s-map-marker"></span>Hồ Chí Minh</div>
+                        <div class="more-info-div date-info"><span class="pe-7s-timer"></span>Hạn nộp: 23/09/2017</div>
+                      </div>
+                </div>
+          </div>
+    </div>
+</div><!-- .job-item -->
+                     */
                     String title = doc.title();
                     builder.append(title).append("\n");
-                    for (Element element : doc.select("a[href^=viec-lam]")) {
-                        builder.append("\n").append("Job title: ").append(element.attr("title"));
+                    for (Element jobTitleElement : doc.select("a[href^=viec-lam]")) {
+                        // jobTitleElement ís an <a> tag
+                        Element jobElement = jobTitleElement.parent() // <h3>
+                                .parent(); // <div class="job-info">
+                        Element companyElement = jobElement.child(1) // <p class="company-name">
+                                .child(0); // <a>
+                        Element infoElement = jobElement.child(3); // <div class="more-info clearfix">
+                        builder.append("\nCông việc: ").append(jobTitleElement.val())
+                                .append("\nLink: ").append(jobTitleElement.attr("href"))
+                                .append("\nDoanh nghiệp: ").append(companyElement.val()));
                     }
-                    /*Elements links = doc.select("a[href]");
-
-                    for (Element link : links) {
-                        builder.append("\n").append("Link : ").append(link.attr("href"))
-                                .append("\n").append("Text : ").append(link.text());
-                    }*/
                 } catch (IOException e) {
                     builder.append("Error : ").append(e.getMessage()).append("\n");
                 }
